fd4a3d7c246601ce9d75cc9be9535ce6784b7fb6
Pomegranate123 <elias@hoste.info>
2022-11-22 16:47:51 +0100

    update 0.04

--- apps/haalarm/boot.js
+++ apps/haalarm/boot.js
@@ -22,9 +22,9 @@ var updateAlarms = function(alarms) {
     if (alarm) {
       var hours = Math.floor(alarm.t / 3600000);
       var mins = (alarm.t % 3600000) / 60000;
-      days.push({ on: alarm.on, hours, mins });
+      days.push({ on: alarm.on, time: `${hours}:${mins}:00` });
     } else {
-      days.push({ on: false, hours: 12, mins: 0});
+      days.push({ on: false, time: "12:00:00"});
     }
   }
   sendTrigger({trigger: "ALARM", days });
