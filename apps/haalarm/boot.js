30f91ef55471dcfb109a42bde4ea8b38df65ad5e
Pomegranate123 <elias@hoste.info>
2022-11-22 16:29:38 +0100

    update 0.03

--- apps/haalarm/boot.js
+++ apps/haalarm/boot.js
@@ -1,20 +1,11 @@
 var sendTrigger = function(data) {
-  var retries = 3;
-  while (retries > 0) {
-    try {
-      Bluetooth.println(); // Make sure nothing else is already on the line we're gonna be printing on
-      Bluetooth.println(JSON.stringify({
-        t:"intent",
-        action:"com.espruino.gadgetbridge.banglejs.HA",
-        extra: data,
-      }));
-      retries = -1;
-      Bangle.buzz();
-    } catch(e) {
-        Bangle.buzz(1000);
-        retries--;
-    }
-  }
+  Bluetooth.println(""); // Make sure nothing else is already on the line we're gonna be printing oni
+  Bluetooth.println(JSON.stringify({
+    t:"intent",
+    action:"com.espruino.gadgetbridge.banglejs.HA",
+    extra: data,
+  }));
+  Bangle.buzz();
 };
 
 require("sched").setAlarms = function(alarms) {
@@ -25,9 +16,9 @@ require("sched").setAlarms = function(alarms) {
 var updateAlarms = function(alarms) {
   var days = [];
   for (var day = 0; day < 7; day++) {
-    var alarm = alarms.filter(a=>
+    var alarm = alarms.filter(a =>
       (a.dow >> day) & 1
-    ).sort(a=>a.t)[0];
+    ).sort((a,b) => a.t - b.t)[0];
     if (alarm) {
       var hours = Math.floor(alarm.t / 3600000);
       var mins = (alarm.t % 3600000) / 60000;
