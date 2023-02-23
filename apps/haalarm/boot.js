6761216068a01ca78766c4ecf213ae7e4ec56f15
Pomegranate123 <elias@hoste.info>
2023-02-23 18:02:44 +0100

    update 0.05

--- apps/haalarm/boot.js
+++ apps/haalarm/boot.js
@@ -1,5 +1,5 @@
 var sendTrigger = function(data) {
-  Bluetooth.println(""); // Make sure nothing else is already on the line we're gonna be printing oni
+  Bluetooth.println(""); // Make sure nothing else is already on the line we're gonna be printing on
   Bluetooth.println(JSON.stringify({
     t:"intent",
     action:"com.espruino.gadgetbridge.banglejs.HA",
@@ -14,18 +14,14 @@ require("sched").setAlarms = function(alarms) {
 };
 
 var updateAlarms = function(alarms) {
-  var days = [];
-  for (var day = 0; day < 7; day++) {
-    var alarm = alarms.filter(a =>
-      (a.dow >> day) & 1
-    ).sort((a,b) => a.t - b.t)[0];
-    if (alarm) {
-      var hours = Math.floor(alarm.t / 3600000);
-      var mins = (alarm.t % 3600000) / 60000;
-      days.push({ on: alarm.on, time: `${hours}:${mins}:00` });
-    } else {
-      days.push({ on: false, time: "12:00:00"});
-    }
+  var data = [];
+  // Select earliest alarm with message "Wekker"
+  var alarm = alarms.filter(a => a.msg == "Wekker"
+  ).sort((a,b) => a.t - b.t)[0];
+  if (alarm) {
+    var hours = Math.floor(alarm.t / 3600000);
+    var mins = (alarm.t % 3600000) / 60000;
+    data.push({ on: alarm.on, time: `${hours}:${mins}:00` });
+    sendTrigger({trigger: "Wekker", data });
   }
-  sendTrigger({trigger: "ALARM", days });
 };
